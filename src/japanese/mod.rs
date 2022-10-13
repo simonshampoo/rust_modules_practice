372952199c2557708e058bb85911dfe717fca280
Simon Shamoon <simonshamoon@Simons-MacBook-Air.local>
2022-10-13 00:18:11 -0500

    first commit

--- src/japanese/mod.rs
+++ src/japanese/mod.rs
@@ -1,3 +1,7 @@
+pub use self::greetings::hello; 
+pub use self::farewells::goodbye; 
+
+
 pub mod farewells; 
 pub mod greetings; 
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate phrases; 
 
+use phrases::english::{greetings, farewells};
+use phrases::japanese;
 fn main() {
-	println!("Hello in English: {}", phrases::english::greetings::hello()); 
-	println!("Hello in Japanese: {}", phrases::english::greetings::hello()); 
-
-	println!("Goodbye in English: {}", phrases::english::farewells::goodbye()); 
-	println!("Goodbye in Japanese: {}", phrases::japanese::farewells::goodbye());
+	println!("Hello in English: {}", greetings::hello()); 
+	println!("Hello in Japanese: {}", japanese::hello()); 
+	println!("Goodbye in English: {}", farewells::goodbye()); 
+	println!("Goodbye in Japanese: {}", japanese::goodbye()); 
 } 
